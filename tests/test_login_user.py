b04fb703dc35682729cf1fb8a0440594b906a055
dea-1611 <dea.ws1611@gmail.com>
2025-08-17 17:10:43 +0500

    Update test_login_user.py

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -4,9 +4,9 @@ from helpers.data_generator import generate_email, generate_password
 from helpers.urls import Urls
 
 
-@allure.feature('User Login')
+@allure.feature('логин пользователя')
 class TestLoginUser:
-    @allure.title('Login with valid credentials')
+    @allure.title('Вход под существующим пользователем')
     def test_login_valid_user(self, api_client, registered_user):
         response = api_client.post(Urls.LOGIN, json={
             'email': registered_user['email'],
@@ -17,10 +17,21 @@ class TestLoginUser:
         assert response.json()['success'] is True
         assert 'accessToken' in response.json()
 
-    @allure.title('Login with invalid credentials')
-    def test_login_invalid_credentials(self, api_client):
+    @allure.title('Вход с неверным логином')
+    def test_login_invalid_email(self, api_client, registered_user):
         response = api_client.post(Urls.LOGIN, json={
             'email': generate_email(),
+            'password': registered_user['password']
+        })
+
+        assert response.status_code == 401
+        assert response.json()['success'] is False
+        assert response.json()['message'] == 'email or password are incorrect'
+
+    @allure.title('Вход с неверным паролем')
+    def test_login_invalid_password(self, api_client, registered_user):
+        response = api_client.post(Urls.LOGIN, json={
+            'email': registered_user['email'],
             'password': generate_password()
         })
 
