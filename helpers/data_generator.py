59f8e5eba402373fd21d8b4d9e99c44c84de7c6b
dea-1611 <dea.ws1611@gmail.com>
2025-08-19 21:48:14 +0500

    Update data_generator.py

--- helpers/data_generator.py
+++ helpers/data_generator.py
@@ -1,3 +1,5 @@
+import time
+import random
 from faker import Faker
 from helpers.urls import Urls
 
@@ -5,7 +7,9 @@ fake = Faker()
 fake_ru = Faker('ru_RU')
 
 def generate_email():
-    return fake.email()
+    timestamp = int(time.time() * 1000)
+    random_num = random.randint(1000, 9999)
+    return f"test_{timestamp}_{random_num}@example.com"
 
 def generate_password():
     return fake.password(length=10, special_chars=True, digits=True)
@@ -16,4 +20,6 @@ def generate_name():
 def get_valid_ingredients(api_client):
     response = api_client.get(Urls.INGREDIENTS)
     response.raise_for_status()
-    return [ingredient['_id'] for ingredient in response.json()['data']][:2]
+    data = response.json()
+    assert 'data' in data, "Unexpected response structure"
+    return [ingredient['_id'] for ingredient in data['data']][:2]
