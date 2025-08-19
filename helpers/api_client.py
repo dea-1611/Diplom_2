bacc518c8000bff8b5d5eff30ff63149ac3ef9eb
dea-1611 <dea.ws1611@gmail.com>
2025-08-19 21:48:00 +0500

    Update api_client.py

--- helpers/api_client.py
+++ helpers/api_client.py
@@ -1,15 +1,4 @@
-import requests
+from helpers.base_api import BaseAPI
 
-
-class APIClient:
-    def __init__(self):
-        self.session = requests.Session()
-
-    def post(self, url, json=None, headers=None):
-        return self.session.post(url, json=json, headers=headers)
-
-    def get(self, url, headers=None):
-        return self.session.get(url, headers=headers)
-
-    def delete(self, url, headers=None):
-        return self.session.delete(url, headers=headers)
+class APIClient(BaseAPI):
+    pass
