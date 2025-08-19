72b06b5ed558bf7e40adc55693b017d2e044c4ff
dea-1611 <dea.ws1611@gmail.com>
2025-08-19 21:51:01 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import pytest
-import allure
+import requests
 from helpers.api_client import APIClient
 from helpers.data_generator import generate_email, generate_password, generate_name
 from helpers.urls import Urls
@@ -18,16 +18,13 @@ def registered_user(api_client):
         'name': generate_name()
     }
 
-    # Регистрация
     response = api_client.post(Urls.REGISTER, json=user_data)
     if response.status_code == 403:
-        # Пользователь уже существует, пробуем с новыми данными
         user_data['email'] = generate_email()
         response = api_client.post(Urls.REGISTER, json=user_data)
 
     assert response.status_code == 200, f"Failed to register user: {response.text}"
 
-    # Логин для получения токена
     login_response = api_client.post(Urls.LOGIN, json={
         'email': user_data['email'],
         'password': user_data['password']
@@ -39,9 +36,12 @@ def registered_user(api_client):
     yield {
         'email': user_data['email'],
         'password': user_data['password'],
+        'name': user_data['name'],
         'token': token
     }
 
-    # Удаление пользователя
     if token:
-        api_client.delete(Urls.USER, headers={'Authorization': token})
+        try:
+            api_client.delete(Urls.USER, headers={'Authorization': token})
+        except requests.exceptions.RequestException:
+            pass
